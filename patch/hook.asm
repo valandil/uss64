9c82c6da1cfefc0cf477ca7d14d2cc6f33fc43e8
Joey Dumont <joey.dumont@gmail.com>
2018-01-26 15:32:09 -0500

    New method of hooking for DMA. Must now fix the hook to the payload.

--- patch/hook.asm
+++ patch/hook.asm
@@ -2,27 +2,33 @@
 // Author:       Joey Dumont                   <joey.dumont@gmail.com>       //
 // Date:         2018-01-15                                                  //
 // Description:  ASM code that links the trainer code in the ROM and then    //
-//               DMAs it and the SM64 initialization function to expansion   //
-//               pak memory.                                                 //
+//               DMAs it to expansion pak memory.                            //
 //                                                                           //
-// We assume that the old initialization function at 0x1000 was already      //
-// copied to 0x007C6C0 by the make-rom.lua LUA script. We then overwrite the //
-// init function at 0x1000 by our own asm that DMAs the old init function and//
-// our own trainer code to the expansion pak memory. After the DMA, we jump  //
-// back to the initialization function that we DMA'd at 0x80400000.          //
+// We hook into the Main (0x1DF8) function of SM64, to DMA copy our payload  //
+// to expansion pak memory. We exploit the fact that the function starting at//
+// 0x80246050 is not used by SM64 to jump to that block of code from Main,   //
+// after osInitalize has been called, to use the DmaCopy function contained  //
+// in SM64 to DMA copy our payload in RAM.                                   //
 // ------------------------------------------------------------------------- //
 
 .n64
-.open "SM64-PracRom.z64", "SM64-PracRom-asm.z64", 0x80245000
 
+// RAM entry point is 0x80245000
+ramEntryPoint equ 0x80245000
+romPadding    equ 0x007CC6C0
+hookPoint     equ 0x1E50
+
+.open "SM64-PracRom.z64", "SM64-PracRom-asm.z64", ramEntryPoint
+
+// Labels to be placed in a separate asm file eventually.
 // void PrintXY(unsigned int x, unsigned int y, const char *str);
 .definelabel PrintXY, 0x802D66C0
 
 // void DmaCopy(unsigned int RAM_offset, unsigned int ROM_bottom, unsigned int ROM_top);
 .definelabel DmaCopy, 0x80278504
 
-// Import hello_world at the end of the ROM.
-.orga 0x007CC6C0 + 0x50
+// Import the payload at the end of the ROM.
+.orga romPadding
 NewCodeVaddrStart:
 .importobj "../hello_world.o"
 NewCodeVaddrEnd:
@@ -31,21 +37,25 @@ NewCodeVaddrEnd:
 // This should DMA from ROM 0x007CC6C0 to whatever size is 0x50 + the linked
 // hello_world.o object. It should then jump back to the old initialization
 // function.
-.orga 0x1000
+.orga hookPoint
+jal 0x80246050
+
+.org 0x80246050
+addiu sp, sp, 0xFFE8
+sw $ra, 0x0014(sp)
 la a0, 0x80400000
-la a1, NewCodeVaddrStart - 0x50
-la a2, NewCodeVaddrEnd
+la a1, NewCodeVaddrStart - ramEntryPoint
+la a2, NewCodeVaddrEnd - ramEntryPoint
 jal DmaCopy
 nop
-j 0x80400000
+lw ra, 0x0014(sp)
+jr ra
+nop
+addiu sp, sp, 0x0018
+nop
 
 // replace unused Mario behavior with the our payload.
 .orga 0x21CCE0
-.dw 0x80400050
+jal 0x80400000
 
 .close
-
-// Other place to execute DMA:
-// 0x514.
-// The culprit here seems to be jal DmaCopy, which doesn't terminate or something.
-// Should check it out in BizHawk.
