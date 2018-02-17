79687c15bdb3135dbf5875b03f32cdeb11189e26
Joey Dumont <joey.dumont@gmail.com>
2018-02-17 15:46:30 -0500

    Further tests.

--- compile.sh
+++ compile.sh
@@ -8,6 +8,13 @@ mips64-gcc -DZ64_VERSION=Z64_OOT10 -G 0 -Wall -O2 -mtune=vr4300 -march=vr4300 -m
 mips64-gcc -DZ64_VERSION=Z64_OOT10 -G 0 -Wall -O2 -mtune=vr4300 -march=vr4300 -mabi=32 -c -I /opt/n64-dev/n64-sysroot/include gz/src/gz/gfx.c       -o gfx.o
 mips64-gcc -DZ64_VERSION=Z64_OOT10 -G 0 -Wall -O2 -mtune=vr4300 -march=vr4300 -mabi=32 -c -I /opt/n64-dev/n64-sysroot/include gz/src/gz/gu.c        -o gu.o
 mips64-gcc -DZ64_VERSION=Z64_OOT10 -G 0 -Wall -O2 -mtune=vr4300 -march=vr4300 -mabi=32 -c -I /opt/n64-dev/n64-sysroot/include gz/src/gz/zu.c        -o zu.o
-mips64-g++ -T /opt/n64-dev/n64-sysroot/lib/gl-n64.ld -nostartfiles -specs=nosys.specs -O3 -flto -Wl,--gc-sections -Wl,--defsym,start=0x80400000 -mabi=32 \
-              hello_world.o resource.o gfx.o gu.o grc.o zu.o vector.o startup.o -o uss64.o
+mips64-g++ -T /opt/n64-dev/n64-sysroot/lib/gl-n64.ld \
+           -nostartfiles \
+           -specs=nosys.specs \
+           -O3 \
+           -flto \
+           -Wl,--gc-sections \
+           -Wl,--defsym,start=0x80400000 \
+           -mabi=32 \
+              hello_world.o resource.o gfx.o gu.o grc.o zu.o vector.o startup.o -o uss64.elf
 mips64-objcopy -O binary uss64.elf uss64.bin
