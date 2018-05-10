4d31c61763d9f1253dec22a568d274afca365ba3
Joey Dumont <joey.dumont@gmail.com>
2018-05-10 16:52:02 -0400

    Minor annoyances.

--- compile.sh
+++ compile.sh
@@ -1,29 +1,33 @@
 cd $(dirname $(readlink -f $0))
 #BIN_ROOT=/opt/n64/bin/
-BIN_ROOT=/opt/n64-dev/usr/bin
-#N64_SYSROOT=/opt/n64/mips64/n64-sysroot/usr
-N64_SYSROOT=/opt/n64-dev/n64-sysroot/
+#BIN_ROOT=/opt/n64-dev/usr/bin
+BIN_ROOT=/usr/bin
+N64_SYSROOT=/opt/n64/mips64/n64-sysroot/usr
+#N64_SYSROOT=/opt/n64-dev/n64-sysroot/
 CFLAGS='-g -Wall -O2 -mtune=vr4300 -march=vr4300 -mabi=32 -DF3D_GBI -DSM64_U'
+CROSS=mips64-elf-
 $BIN_ROOT/grc  gz/res/gz/fipps.png -d gz/res/resources.json -o fipps.png.o
-$BIN_ROOT/mips64-gcc                         $CFLAGS -c                         $N64_SYSROOT/include/grc.c                   -o grc.o
-$BIN_ROOT/mips64-gcc                         $CFLAGS -c                         $N64_SYSROOT/include/vector/vector.c         -o vector.o
-$BIN_ROOT/mips64-gcc                         $CFLAGS -c                         $N64_SYSROOT/include/startup.c               -o startup.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include uss64.c                                      -o uss64.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include sm64.c                                       -o sm64.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/resource.c                         -o resource.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/gfx.c                              -o gfx.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/gu.c                               -o gu.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/zu.c                               -o zu.o
-$BIN_ROOT/mips64-g++                                                         \
+$BIN_ROOT/${CROSS}gcc                         $CFLAGS -c                         $N64_SYSROOT/include/grc.c                   -o grc.o
+$BIN_ROOT/${CROSS}gcc                         $CFLAGS -c                         $N64_SYSROOT/include/vector/vector.c         -o vector.o
+$BIN_ROOT/${CROSS}gcc                         $CFLAGS -c                         $N64_SYSROOT/include/startup.c               -o startup.o
+$BIN_ROOT/${CROSS}gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include uss64.c                                      -o uss64.o
+$BIN_ROOT/${CROSS}gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include sm64.c                                       -o sm64.o
+$BIN_ROOT/${CROSS}gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/resource.c                         -o resource.o
+$BIN_ROOT/${CROSS}gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/gfx.c                              -o gfx.o
+$BIN_ROOT/${CROSS}gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/gu.c                               -o gu.o
+$BIN_ROOT/${CROSS}gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/zu.c                               -o zu.o
+$BIN_ROOT/${CROSS}g++                                                         \
            -Wa,-adhln                                                        \
            -T ${N64_SYSROOT}/lib/gl-n64.ld                  \
            -nostartfiles                                                     \
            -specs=nosys.specs                                                \
-           -O3                                                               \
+           -O2                                                               \
+           -Wall                                                             \
+           -mtune=vr4300 -march=4300                                         \
            -flto                                                             \
            -Wl,--gc-sections                                                 \
            -Wl,--defsym,start=0x80400000                                     \
            -mabi=32                                                          \
            uss64.o sm64.o resource.o gfx.o gu.o grc.o zu.o vector.o startup.o \
            -o uss64.elf
-$BIN_ROOT/mips64-objcopy -O binary uss64.elf uss64.bin
+$BIN_ROOT/${CROSS}objcopy -O binary uss64.elf uss64.bin
--- uss64.c
+++ uss64.c
@@ -9,14 +9,6 @@
 
 #include "sm64.h"
 
-void  (*PrintXY)           (unsigned int x, unsigned int y, const char *str) = (void*)PrintXY_addr;
-void  (*sm64_printf)       (int x, int y, const char *format, ...)           = (void*)sm64_printf_addr;
-int   (*GetSegmentBase)    (int segment)                                     = (void*)GetSegmentBase_addr;
-void* (*SegmentedToVirtual)(void* addr)                                      = (void*)SegmentedToVirtual_addr;
-void* (*alloc_displaylist) (unsigned int size)                               = (void*)alloc_displaylist_addr;
-void  (*func_0x8024784C)   ()                                                = (void*)func_0x8024784C_addr;
-
-
 static const char HelloString[] = "hello n64";
 static unsigned int x = 64;
 static unsigned int y = 32;
