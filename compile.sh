bad10b3d0ae254181123165bb4a0d3a6b00cb74b
Joey Dumont <joey.dumont@gmail.com>
2018-05-10 10:16:49 -0400

    TEsting.

--- compile.sh
+++ compile.sh
@@ -1,20 +1,22 @@
 cd $(dirname $(readlink -f $0))
-BIN_ROOT=/opt/n64/bin/
-N64_SYSROOT=/opt/n64/mips64/n64-sysroot/
+#BIN_ROOT=/opt/n64/bin/
+BIN_ROOT=/opt/n64-dev/usr/bin
+#N64_SYSROOT=/opt/n64/mips64/n64-sysroot/usr
+N64_SYSROOT=/opt/n64-dev/n64-sysroot/
 CFLAGS='-g -Wall -O2 -mtune=vr4300 -march=vr4300 -mabi=32 -DF3D_GBI -DSM64_U'
-$BIN_ROOT/grc gz/res/gz/fipps.png -d gz/res/resources.json -o fipps.png.o
-$BIN_ROOT/mips64-gcc                         $CFLAGS -c                             $N64_SYSROOT/usr/include/grc.c           -o grc.o
-$BIN_ROOT/mips64-gcc                         $CFLAGS -c                             $N64_SYSROOT/usr/include/vector/vector.c -o vector.o
-$BIN_ROOT/mips64-gcc                         $CFLAGS -c                             $N64_SYSROOT/usr/include/startup.c       -o startup.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/usr/include uss64.c                                  -o uss64.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/usr/include sm64.c                                   -o sm64.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/usr/include gz/src/gz/resource.c                     -o resource.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/usr/include gz/src/gz/gfx.c                          -o gfx.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/usr/include gz/src/gz/gu.c                           -o gu.o
-$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/usr/include gz/src/gz/zu.c                           -o zu.o
+$BIN_ROOT/grc  gz/res/gz/fipps.png -d gz/res/resources.json -o fipps.png.o
+$BIN_ROOT/mips64-gcc                         $CFLAGS -c                         $N64_SYSROOT/include/grc.c                   -o grc.o
+$BIN_ROOT/mips64-gcc                         $CFLAGS -c                         $N64_SYSROOT/include/vector/vector.c         -o vector.o
+$BIN_ROOT/mips64-gcc                         $CFLAGS -c                         $N64_SYSROOT/include/startup.c               -o startup.o
+$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include uss64.c                                      -o uss64.o
+$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include sm64.c                                       -o sm64.o
+$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/resource.c                         -o resource.o
+$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/gfx.c                              -o gfx.o
+$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/gu.c                               -o gu.o
+$BIN_ROOT/mips64-gcc -DZ64_VERSION=Z64_OOT10 $CFLAGS -c -I $N64_SYSROOT/include gz/src/gz/zu.c                               -o zu.o
 $BIN_ROOT/mips64-g++                                                         \
            -Wa,-adhln                                                        \
-           -T /opt/n64/mips64/n64-sysroot/usr/lib/gl-n64.ld                  \
+           -T ${N64_SYSROOT}/lib/gl-n64.ld                  \
            -nostartfiles                                                     \
            -specs=nosys.specs                                                \
            -O3                                                               \
--- sm64.c
+++ sm64.c
@@ -1,9 +1,9 @@
 #include "sm64.h"
 
 // Function declarations.
-void  (*PrintXY)           (unsigned int x, unsigned int y, const char *str)= (void*)PrintXY_addr
-void  (*sm64_printf)       (int x, int y, const char *format, ...)          = (void*)sm64_printf_addr
-int   (*GetSegmentBase)    (int segment)                                    = (void*)GetSegmentBase_addr
-void* (*SegmentedToVirtual)(void* addr)                                     = (void*)SegmentedToVirtual_addr
-void* (*alloc_displaylist) (unsigned int size)                              = (void*)alloc_displaylist_addr
-void  (*func_0x8024784C)   ()                                               = (void*)func_0x8024784C_addr
+void  (*PrintXY)           (unsigned int x, unsigned int y, const char *str)= (void*)PrintXY_addr;
+void  (*sm64_printf)       (int x, int y, const char *format, ...)          = (void*)sm64_printf_addr;
+int   (*GetSegmentBase)    (int segment)                                    = (void*)GetSegmentBase_addr;
+void* (*SegmentedToVirtual)(void* addr)                                     = (void*)SegmentedToVirtual_addr;
+void* (*alloc_displaylist) (unsigned int size)                              = (void*)alloc_displaylist_addr;
+void  (*func_0x8024784C)   ()                                               = (void*)func_0x8024784C_addr;
